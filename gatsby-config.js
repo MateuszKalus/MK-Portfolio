edceeeb1fcc72c8205d6263856378befd3c351c0
Mateusz Kalus <mat.kalus@gmail.com>
2021-01-06 13:39:58 +0100

    sitemap details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: "MK-portfolio",
+    siteUrl: `https://www.example.com`,
+    siteName: 'Mateusz Kalus\' Portfolio',
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap"],
 };
